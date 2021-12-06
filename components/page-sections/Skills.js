9e70607a9fe14d4af191c13d3f54fb2b8518fb0a
canneth <CHJrWithaC@gmail.com>
2021-12-06 23:32:28 +0800

    Coding style improvements

--- components/page-sections/Skills.js
+++ components/page-sections/Skills.js
@@ -22,41 +22,41 @@ function Skills() {
         <div ref={frontendRef} className={styles.categoryContainer}>
           <p className={styles.listHeader}>Frontend</p>
           <ol className={styles.list}>
-            <li className={styles.listItem}><TechItem for='react' iconWidthExpression='28px' /></li>
-            <li className={styles.listItem}><TechItem for='redux' iconWidthExpression='28px' /></li>
+            <li className={styles.listItem}><TechItem for='react' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
+            <li className={styles.listItem}><TechItem for='redux' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
           </ol>
         </div>
         <div ref={backendRef} className={styles.categoryContainer}>
           <p className={styles.listHeader}>Backend</p>
           <ol className={styles.list}>
-            <li className={styles.listItem}><TechItem for='nodejs' iconWidthExpression='28px' /></li>
-            <li className={styles.listItem}><TechItem for='postgresql' iconWidthExpression='28px' /></li>
+            <li className={styles.listItem}><TechItem for='nodejs' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
+            <li className={styles.listItem}><TechItem for='postgresql' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
           </ol>
         </div>
         <div ref={fullstackRef} className={styles.categoryContainer}>
           <p className={styles.listHeader}>Fullstack</p>
           <ol className={styles.list}>
-            <li className={styles.listItem}><TechItem for='nextjs' iconWidthExpression='28px' /></li>
+            <li className={styles.listItem}><TechItem for='nextjs' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
           </ol>
         </div>
         <div ref={testingRef} className={styles.categoryContainer}>
           <p className={styles.listHeader}>Testing</p>
           <ol className={styles.list}>
-            <li className={styles.listItem}><TechItem for='jest' iconWidthExpression='28px' /></li>
-            <li className={styles.listItem}><TechItem for='testingLibrary' iconWidthExpression='28px' /></li>
+            <li className={styles.listItem}><TechItem for='jest' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
+            <li className={styles.listItem}><TechItem for='testingLibrary' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
           </ol>
         </div>
         <div ref={verControlRef} className={styles.categoryContainer}>
           <p className={styles.listHeader}>Version Control</p>
           <ol className={styles.list}>
-            <li className={styles.listItem}><TechItem for='git' iconWidthExpression='28px' /></li>
-            <li className={styles.listItem}><TechItem for='gitHub' iconWidthExpression='28px' /></li>
+            <li className={styles.listItem}><TechItem for='git' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
+            <li className={styles.listItem}><TechItem for='gitHub' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
           </ol>
         </div>
         <div ref={designRef} className={styles.categoryContainer}>
           <p className={styles.listHeader}>Design</p>
           <ol className={styles.list}>
-            <li className={styles.listItem}><TechItem for='figma' iconWidthExpression='28px' /></li>
+            <li className={styles.listItem}><TechItem for='figma' iconWidthExpression='clamp(28px, 2.5vw, 42px)' /></li>
           </ol>
         </div>
       </div>
