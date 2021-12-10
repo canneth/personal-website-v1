e479e61d09517a03303a3d95177896733cae259e
chjrwithac <chjrwithac@gmail.com>
2021-12-10 13:03:17 +0800

    Housekeeping and reorganising

---
+++ page-sections/contact-section/ContactMe.js
@@ -0,0 +1,12 @@
+
+import styles from './ContactMe.module.css';
+
+function ContactMe() {
+  return (
+    <section className={styles.overallContainer}>
+      
+    </section>
+  );
+}
+
+export default ContactMe;
---
+++ page-sections/hero-section/Hero.js
@@ -0,0 +1,37 @@
+
+import Image from 'next/image';
+
+import ContactBar from '@/components/contact-bar/ContactBar';
+import Button from '@/components/common/Button';
+
+import styles from './Hero.module.css';
+
+function Hero() {
+  return (
+    <section className={styles.overallContainer}>
+      <div className={styles.foregroundContainer}>
+        <p className={styles.subtitle}>{`Hi! I'm`}</p>
+        <h1 className={styles.title}>
+          <span className={styles.titlePrefix}>Ca</span>
+          <span className={styles.titleSuffix}>nneth</span>
+        </h1>
+        <p className={styles.description}>
+          I love creating beautiful solutions to real-world problems
+        </p>
+        <ContactBar className={styles.contactBar}/>
+        <Button className={styles.resumeButton} icon='resume' text='Resume' />
+      </div>
+      <div className={styles.backgroundContainer}>
+        <Image
+          className={styles.backgroundImage}
+          src='/hero-background.svg'
+          alt='Background'
+          layout='fill'
+          priority
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
