692a093f0c63e887644bd9dc131b2f6b49856214
chjrwithac <chjrwithac@gmail.com>
2021-12-10 13:25:46 +0800

    Contact section done for now

--- page-sections/contact-section/ContactMe.js
+++ page-sections/contact-section/ContactMe.js
@@ -1,10 +1,32 @@
 
+import SectionTitle from '@/components/common/SectionTitle';
+import Button from '@/components/common/Button';
 import styles from './ContactMe.module.css';
 
 function ContactMe() {
   return (
     <section className={styles.overallContainer}>
-      
+      <SectionTitle className={styles.sectionTitle}>
+        Let's Chat!
+      </SectionTitle>
+      <div className={styles.contentContainer}>
+        <p className={styles.text}>
+          If you have an enticing opportunity to share, a burning question to ask,
+          or just simply wish to say hello, my inbox is always open.
+          <br /><br />
+          I am currently actively seeking job opportunities, so if I've piqued your
+          interest, my resume is available for download as well.
+          <br /><br />
+          Feel free to drop me a line at
+          <br /><br />
+        </p>
+        <a className={styles.emailLink} href=''>canneth@gmail.com</a>
+        <p className={styles.text}>
+          <br />
+          and I will do my best to get back to you!
+        </p>
+        <Button className={styles.resumeButton} icon='resume' text='Resume' />
+      </div>
     </section>
   );
 }
