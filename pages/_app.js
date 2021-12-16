7874e061ebf8d03fe21ec3ef2512000f6b956236
chjrwithac <chjrwithac@gmail.com>
2021-12-16 10:40:51 +0800

    Added favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 
+import Head from 'next/head';
 import Script from 'next/script';
 
 import Layout from '@/components/layout/Layout';
@@ -9,6 +10,17 @@ import '@/styles/global.css'
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/generated/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/generated/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/generated/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/generated/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/generated/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="apple-mobile-web-app-title" content="Canneth" />
+        <meta name="application-name" content="Canneth" />
+        <meta name="msapplication-TileColor" content="#282828" />
+        <meta name="theme-color" content="#282828" />
+      </Head>
       <Script src='https://code.iconify.design/2/2.1.0/iconify.min.js' />
       <LoadingPage />
       <Layout>
