2f6bd69a5d364ba90ca5096d4e223efea4291a58
chjrwithac <chjrwithac@gmail.com>
2021-12-16 10:50:36 +0800

    Added site title

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Canneth</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/generated/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/generated/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/generated/favicon-16x16.png" />
