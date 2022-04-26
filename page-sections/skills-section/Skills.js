ccb4657897f41890af889cfaa1a88170ff89d69b
canneth <CHJrWithaC@gmail.com>
2022-04-26 15:35:09 +0800

    feat: Add new backend skill postgres

--- page-sections/skills-section/Skills.js
+++ page-sections/skills-section/Skills.js
@@ -33,6 +33,7 @@ function Skills(props) {
           <ol className={styles.list}>
             <li className={styles.listItem}><TechItem for='nodejs' iconWidthExpression='clamp(22px, 5vw, 30px)' /></li>
             <li className={styles.listItem}><TechItem for='expressjs' iconWidthExpression='clamp(22px, 5vw, 30px)' /></li>
+            <li className={styles.listItem}><TechItem for='postgresql' iconWidthExpression='clamp(22px, 5vw, 30px)' /></li>
           </ol>
         </div>
         <div ref={fullstackRef} className={styles.categoryContainer}>
