7d37be221e701e1b28bfa4b1d1195c840e624011
canneth <CHJrWithaC@gmail.com>
2022-05-14 00:51:40 +0800

    feat: Add vue to skills

--- page-sections/skills-section/Skills.js
+++ page-sections/skills-section/Skills.js
@@ -25,6 +25,7 @@ function Skills(props) {
           <h1 className={styles.listHeader}>Frontend</h1>
           <ol className={styles.list}>
             <li className={styles.listItem}><TechItem for='react' iconWidthExpression='clamp(22px, 5vw, 30px)' /></li>
+            <li className={styles.listItem}><TechItem for='vue' iconWidthExpression='clamp(22px, 5vw, 30px)' /></li>
             <li className={styles.listItem}><TechItem for='mobx' iconWidthExpression='clamp(22px, 5vw, 30px)' /></li>
           </ol>
         </div>
